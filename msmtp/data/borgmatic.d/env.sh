b9f608111906d4d12010e38474f1765a3142d623
Frieder Schrempf <frieder@fris.de>
2022-03-13 14:46:58 +0100

    Add note to automatically created files

--- msmtp/data/borgmatic.d/env.sh
+++ msmtp/data/borgmatic.d/env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cat >/etc/mailenv << EOF
+# THIS FILE GETS RECREATED AUTOMATICALLY ON CONTAINER STARTUP
 BACKUP_COMMAND="/usr/bin/borgmatic --stats -v 0"
 MAILTO="${MAIL_TO}"
 MAILSUBJECT="${MAIL_SUBJECT}"
