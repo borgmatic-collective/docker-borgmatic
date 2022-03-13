4a3f1d41d16053d9ef8c73c778d4577540cba5a8
Frieder Schrempf <frieder@fris.de>
2022-03-13 14:38:17 +0100

    Send e-mail reports with more appealing subject line

--- msmtp/data/borgmatic.d/run.sh
+++ msmtp/data/borgmatic.d/run.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
 source /etc/mailenv
-$BACKUP_COMMAND 2>&1
+LOGFILE="/tmp/backup_run_$(date +%s).log"
+
+set -o pipefail
+$BACKUP_COMMAND 2>&1 | tee $LOGFILE
+
+if [ $? -eq "0" ]; then
+    SUBJECT_PREFIX="=?utf-8?Q? =E2=9C=85 SUCCESS?="
+else
+    SUBJECT_PREFIX="=?utf-8?Q? =E2=9D=8C FAILED?="
+fi
+
+if [ -n "$MAILTO" ]; then
+    echo -e "Subject: $SUBJECT_PREFIX: $MAILSUBJECT\n\n$(cat $LOGFILE)\n" |
+        sendmail -t $MAILTO
+fi
+
+rm $LOGFILE
