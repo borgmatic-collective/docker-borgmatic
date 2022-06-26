3a2d5b5f4a63dfb84ec56e9250849266395e9a76
Konnichy <katakayann@gmail.com>
2022-06-26 20:52:57 +0200

    Script files moved out of the bind-mountable configuration directory

--- msmtp/entry.sh
+++ msmtp/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/bin/sh /etc/borgmatic.d/msmtprc.sh
-/bin/sh /etc/borgmatic.d/env.sh
+/bin/sh /scripts/msmtprc.sh
+/bin/sh /scripts/env.sh
 
 # Import your cron file
 /usr/bin/crontab /etc/borgmatic.d/crontab.txt
