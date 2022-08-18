8f5faf5bdf98da41d202bd3a31a0f274d29753a8
modem7 <modem7@gmail.com>
2022-08-18 18:26:58 +0100

    Crontab changes
    Added ENV vars for crontab, removed old file/way of doing things.
    
    Two new ENV vars have been added:
    
    CRON
    CRON_COMMAND
    
    If these are unset, they will default to Borg defaults.
    
    Example usage:
        environment:
          - CRON=0 5 * * *
          - CRON_COMMAND=borgmatic --stats -v 0 2>&1

--- base/entry.sh
+++ base/entry.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-# Import your cron file
-/usr/bin/crontab /etc/borgmatic.d/crontab.txt
+# Generate Cron variables
+export CRON=${CRON:-"0 1 * * *"}
+export CRON_COMMAND=${CRON_COMMAND:-"borgmatic --stats -v 0 2>&1"}
 
-#Variables
+# Output cron settings to console
+echo "Cron job set as: \"$CRON $CRON_COMMAND\""
+
+# Version variables
 borgver=$(borg --version)
 borgmaticver=$(borgmatic --version)
 apprisever=$(apprise --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 
-#Software versions
+# Software versions
 echo borgmatic $borgmaticver
 echo $borgver
 echo apprise $apprisever
 
 # Start cron
+echo "$CRON $CRON_COMMAND" > /etc/crontabs/root
 /usr/sbin/crond -f -L /dev/stdout
