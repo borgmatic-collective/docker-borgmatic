6532bd1329d848fb9d3c9d4d5aaa38a36a663a27
bengel5 <bengel5@emich.edu>
2023-11-15 13:53:34 -0500

    modified for me

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 # syntax = docker/dockerfile:latest
 FROM python:3.11.5-alpine3.18
 LABEL maintainer='github.com/borgmatic-collective'
-VOLUME /mnt/source
-VOLUME /mnt/borg-repository
-VOLUME /root/.borgmatic
-VOLUME /etc/borgmatic.d
-VOLUME /root/.config/borg
-VOLUME /root/.ssh
-VOLUME /root/.cache/borg
+# VOLUME /mnt/source
+# VOLUME /mnt/borg-repository
+# VOLUME /root/.borgmatic
+# VOLUME /etc/borgmatic.d
+# VOLUME /root/.config/borg
+# VOLUME /root/.ssh
+# VOLUME /root/.cache/borg
 RUN apk add --update --no-cache \
     bash \
     bash-completion \
@@ -28,14 +28,22 @@ RUN apk add --update --no-cache \
     sshfs \
     supercronic \
     tzdata \
+    msmtp \
     && rm -rf \
     /var/cache/apk/* \
     /.cache
 
 COPY --chmod=755 entry.sh /entry.sh
 COPY requirements.txt /
+COPY Bengel+CA.crt /usr/local/share/ca-certificates/bengelca.crt
 
 RUN python3 -m pip install --no-cache -Ur requirements.txt
 RUN borgmatic --bash-completion > /usr/share/bash-completion/completions/borgmatic && echo "source /etc/bash/bash_completion.sh" > /root/.bashrc
+
+RUN mkdir /root/.ssh \
+    && touch /root/.ssh/config \
+    && echo "StrictHostKeyChecking=accept-new" | tee /root/.ssh/config \
+    && ln -sf /usr/bin/msmtp /usr/sbin/sendmail \
+    && update-ca-certificates
 
 ENTRYPOINT ["/entry.sh"]
--- entry.sh
+++ entry.sh
@@ -1,103 +1,22 @@
 #!/bin/sh
-
-# Path
-CRONTAB_PATH="/etc/borgmatic.d/crontab.txt"
-
-#Variables
-borgver=$(borg --version)
-borgmaticver=$(borgmatic --version)
-apprisever=$(apprise --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
-
-#Software versions
-echo borgmatic $borgmaticver
-echo $borgver
-echo apprise $apprisever
-
-# Enable initial debug logging based on the DEBUG_SECRETS environment variable.
-# Logs the initial values of BORG_PASSPHRASE and BORG_PASSPHRASE_FILE.
-if [ "${DEBUG_SECRETS}" = "true" ] || [ "${DEBUG_SECRETS}" = "1" ]; then
-  echo "Before: BORG_PASSPHRASE: ${BORG_PASSPHRASE}"
-  echo "Before: BORG_PASSPHRASE_FILE: ${BORG_PASSPHRASE_FILE}"
-fi
-
-# Loop through all environment variables that start with 'BORG'.
-for var_name in $(set | grep '^BORG' | awk -F= '{print $1}'); do
-  # Retrieve the current value of each environment variable.
-  var_value=$(eval echo \$$var_name)
-
-  # Check if the variable's name ends with '_FILE'.
-  if [[ "$var_name" =~ _FILE$ ]]; then
-    # Strip the '_FILE' suffix to obtain the corresponding variable name.
-    original_var_name=${var_name%_FILE}
-
-    # Retrieve the value of the original environment variable, if it exists.
-    original_var_value=$(eval echo \$$original_var_name)
-
-    # Ensure the *_FILE variable is valid, and the referenced file exists and is not empty.
-    if [ -n "$var_value" ] && [ -s "$var_value" ]; then
-      # Notify user if original variable is being overwritten.
-      if [ -n "$original_var_value" ]; then
-        echo "Note: $original_var_name was already set but is being overwritten by $var_name"
-      fi
-
-      # Update the original variable with the content of the file.
-      export "$original_var_name"=$(cat "$var_value")
-      echo "Setting $original_var_name from the content of $var_value"
-
-      # Unset the *_FILE environment variable.
-      unset "$var_name"
-      echo "Unsetting $var_name"
-    else
-      # Issue an error if the *_FILE variable is not properly set, or the file does not exist or is empty.
-      echo "Error: File $var_value does not exist or is empty."
-    fi
-  fi
-done
-
-# Enable final debug logging based on the DEBUG_SECRETS environment variable.
-# Logs the final values of BORG_PASSPHRASE and BORG_PASSPHRASE_FILE.
-if [ "${DEBUG_SECRETS}" = "true" ] || [ "${DEBUG_SECRETS}" = "1" ]; then
-  echo "After: BORG_PASSPHRASE: ${BORG_PASSPHRASE}"
-  echo "After: BORG_PASSPHRASE_FILE: ${BORG_PASSPHRASE_FILE}"
-fi
-
-if [ $# -eq 0 ]; then
-
-  # Allow setting of custom crontab, so check if crontab file exists
-  if [ -f "$CRONTAB_PATH" ]; then
-    echo "Crontab file exists, using it"
-  else
-    if [ -z "${BACKUP_CRON}" ]; then
-      echo "Environment variable BACKUP_CRON is not set, using default value: 0 1 * * *"
-      export BACKUP_CRON="0 1 * * *"
-    else
-      echo "Environment variable BACKUP_CRON is set, using value $BACKUP_CRON"
-    fi
-    echo "$BACKUP_CRON PATH=\$PATH:/usr/local/bin /usr/local/bin/borgmatic --stats -v 0 2>&1" > $CRONTAB_PATH
-  fi
-
-  if [ "${RUN_ON_STARTUP:-}" == "true" ]; then
-    echo "Running on startup..."
-    /usr/local/bin/borgmatic --stats -v 0 2>&1
-  fi
-
-  # Test crontab
-  supercronic -test /etc/borgmatic.d/crontab.txt || exit 1
-
-  # Start supercronic
-  if [ -n "${SUPERCRONIC_EXTRA_FLAGS}" ]; then
-    echo "The variable SUPERCRONIC_EXTRA_FLAGS is not empty, using extra flags"
-    exec supercronic $SUPERCRONIC_EXTRA_FLAGS /etc/borgmatic.d/crontab.txt
-  else
-    echo "The variable SUPERCRONIC_EXTRA_FLAGS is empty, starting normally"
-    exec supercronic /etc/borgmatic.d/crontab.txt
-  fi
-else
-  if [ "$1" = "bash" ] || [ "$1" = "sh" ] || [ "$1" = "/bin/bash" ] || [ "$1" = "/bin/sh" ]; then
-    # Run Shell
-    exec "$@"
-  else
-    # Run borgmatic with subcommand
-    exec borgmatic "$@"
-  fi
-fi
+# timezone
+/bin/echo $TZ > /etc/timezone
+# setup msmtp
+#!/bin/sh
+cat >/etc/msmtprc << EOF
+# Set default values for all following accounts.
+defaults
+# auth             on
+# tls              on
+# tls_starttls	 on
+tls_trust_file   /etc/ssl/certs/ca-certificates.crt
+logfile		     /dev/stdout
+account	default
+host ${MAIL_RELAY_HOST}
+port ${MAIL_PORT}
+from ${MAIL_FROM}
+user ${MAIL_USER}
+password ${MAIL_PASSWORD}
+EOF
+# run borgmatic
+/usr/local/bin/borgmatic --log-file /dev/stdout --log-file-verbosity 2 2>&1
